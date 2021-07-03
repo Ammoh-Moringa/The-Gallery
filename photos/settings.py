62c8552dc592d7b8600dc14f29f2f5d4c89a46eb
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-03 15:57:21 +0300

    Add class category

--- photos/settings.py
+++ photos/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'photos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'moringa',
+    'PASSWORD':'Ammoh',
+    }
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
