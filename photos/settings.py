10acf7ad5f7b2e239767f2bf137147ba9919d0cb
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-03 15:49:08 +0300

    Add psycopg2 to connect postgress to photos

--- photos/settings.py
+++ photos/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Gallery.apps.GalleryConfig'
+    'Gallery.apps.GalleryConfig',
     'bootstrap3',
 ]
 
