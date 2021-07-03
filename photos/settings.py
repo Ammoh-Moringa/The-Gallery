92d5004ebb39af548b26df4b1a673a850facb5a0
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-03 13:53:42 +0300

    Registered Gallery app and bootstrap in installed apps

--- photos/settings.py
+++ photos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Gallery.apps.GalleryConfig'
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
