089a2c731c6803f0d2c54a5febc8e3209bfac658
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-05 01:35:01 +0300

    set up test for location category and location

--- Gallery/tests.py
+++ Gallery/tests.py
@@ -3,3 +3,14 @@ from django.test import TestCase
 from .models import Image, Category, Location
 
 # Create your tests here.
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Barcelona')
+        self.location.save_location()
+
+        self.category = Category(name='Sports')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='Barcelona', description='Barcelona football club', location=self.location,
+                                category=self.category)
