20b176eb7046b10949fc606d38d832ebb881e3db
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-05 02:08:11 +0300

    Test for all behaviors in all categories

--- Gallery/tests.py
+++ Gallery/tests.py
@@ -35,7 +35,61 @@ class TestImage(TestCase):
         self.assertTrue(len(changed_img) > 0)
 
      
-    def test_get_image_by_id(self):
-            found_image = self.image.get_image_by_id(self.image.id)
-            images = Image.objects.filter(id=self.image.id)
-            self.assertFalse(found_image, images)
+    # def test_search_image_by_category(self):
+    #     category = 'sports'
+    #     found_img = self.image_test.search_by_category(category)
+    #     self.assertTrue(len(found_img) > 1)
+        
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(name='Moringa')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    # def test_get_locations(self):
+    #     self.location.save_location()
+    #     locations = Location.get_locations()
+    #     self.assertTrue(len(locations) > 1)
+
+    # def test_update_location(self):
+    #     new_location = 'kericho'
+    #     self.location.update_location(self.location.id, new_location)
+    #     changed_location = Location.objects.filter(name='kericho')
+    #     self.assertTrue(len(changed_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(name='home')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
